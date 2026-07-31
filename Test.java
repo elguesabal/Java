deaf677cfbaabb55fe11c12bb4796b42ee118942
elguesabal <joseelguesabal@gmail.com>
2026-07-31 20:07:25 -0300

    aprendendo java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+	private static void printArgs(String[] args) {
+		System.out.println(args[0]);
+	}
+	public static void main(String[] args) {
+		System.out.println("--- Test ---");
+		printArgs(args);
+	}
+}
