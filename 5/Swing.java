8cc1ec4ecb643f5b1a6df3ec50d55dd5a681b7bb
elguesabal <joseelguesabal@gmail.com>
2026-08-01 22:56:40 -0300

    aprendendo java

---
+++ 5/Swing.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class Swing {
+	public static void main(String[] args) {
+		JFrame 	window = new JFrame("Window");
+		JLabel 	text = new JLabel("Vampeta");
+
+		window.add(text);
+		window.setSize(400, 200);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
+}
