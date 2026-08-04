9fb49828eb9f3392fada4dca97b9caba1863b8b8
elguesabal <joseelguesabal@gmail.com>
2026-08-04 20:03:47 -0300

    aprendendo java

--- 5/JavaFX1.java
+++ 5/JavaFX1.java
@@ -1,12 +1,12 @@
-// import javafx.application.Application;
-// import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-// public class JavaFX1 extends Application {
-// 	@Override
-// 	public void start(Stage stage) {
-// 		stage.show();
-// 	}
-// 	public static void main(String[] args) {
-// 		launch();
-// 	}
-// }
+public class JavaFX1 extends Application {
+	@Override
+	public void start(Stage stage) {
+		stage.show();
+	}
+	public static void main(String[] args) {
+		launch();
+	}
+}
---
+++ 6/Variables.java
@@ -0,0 +1,28 @@
+public class Variables {
+	public static void main(String[] args) {
+		int			int1 = 42;
+		float		float1 = 42.42f;
+		char		char1 = 'a';
+		boolean		boolean1 = true;
+
+		int			int2 = (int) 42;
+		float		float2 = (float) 42.42f;
+		char		char2 = (char) 'a';
+		boolean		boolean2 = (boolean) true;
+
+		Integer		int3 = new Integer(42);
+		Float		float3 = new Float(42.42);
+		Character	char3 = new Character('a');
+		Boolean		boolean3 = new Boolean(true);
+
+		Integer		int4 = Integer.valueOf(42);
+		Float		float4 = Float.valueOf(42.42f);
+		Character	char4 = Character.valueOf('a');
+		Boolean		boolean4 = Boolean.valueOf(true);
+
+		Integer		int5 = 42;
+		Float		float5 = 42.42f;
+		Character	char5 = 'a';
+		Boolean		boolean5 = true;
+	}
+}
