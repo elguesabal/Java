55c4863f533f67a8dc442cd431dc94d35e61a7e5
elguesabal <joseelguesabal@gmail.com>
2026-08-06 13:37:04 -0300

    estudando java

---
+++ 7/Operators.java
@@ -0,0 +1,27 @@
+public class Operators {
+	public static void main(String[] args) {
+		int	n = 42;
+
+		System.out.println(42 + 42);
+		System.out.println(42 - 42);
+		System.out.println(42 * 2);
+		System.out.println(42 / 2);
+		System.out.println(42 % 2);
+		System.out.println("Vampeta " + "42");
+		System.out.println("Vampeta " + 42);
+		System.out.println(n++);
+		System.out.println(n--);
+		System.out.println(++n);
+		System.out.println(--n);
+		n += 1;
+		System.out.println(n);
+		n -= 1;
+		System.out.println(n);
+		n *= 2;
+		System.out.println(n);
+		n /= 2;
+		System.out.println(n);
+		n %= 2;
+		System.out.println(n);
+	}
+}
